07e8276b9d5f6f39bfdd7d0381d30f873eecdc51
Rohit <chauhan9911910013@gmail.com>
2022-07-11 15:08:35 +0000

    Module 4 completed

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required,StringLength(100,MinimumLength =8),DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, DataType(DataType.Password), CompareAttribute("Password",ErrorMessage = "The New Password and Confirm New Password fields did not match.")]
+        public string ConfirmPassword { get; set; } 
+
+    }
+}
