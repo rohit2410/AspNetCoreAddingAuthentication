7707620459dece32deb1cbfac10a16cb13465e3c
rohit2410 <75296024+rohit2410@users.noreply.github.com>
2022-07-15 18:04:44 +0530

    Last change

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ namespace WishList.Controllers
     [Authorize]
     public class AccountController : Controller
     {
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
